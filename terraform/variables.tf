a05133f59619bed5ffc16f4699d9998eb6a2eb4f
Prathamesh Sawant <104883046+Prathamesh78@users.noreply.github.com>
2024-06-14 11:45:40 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "rds_db_name" {
+  description = "The name of the RDS database"
+  type        = string
+}
+
+variable "rds_username" {
+  description = "The username for the RDS database"
+  type        = string
+}
+
+variable "rds_password" {
+  description = "The password for the RDS database"
+  type        = string
+}
