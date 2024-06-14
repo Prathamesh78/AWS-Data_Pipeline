44d8a292820fe64d632161ab4055764b2073ef3e
Prathamesh Sawant <104883046+Prathamesh78@users.noreply.github.com>
2024-06-14 12:23:18 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,12 @@
+variable "aws_access_key_id" {
+  description = "The AWS access key ID"
+  type        = string
+}
+
+variable "aws_secret_access_key" {
+  description = "The AWS secret access key"
+  type        = string
+}
 variable "rds_db_name" {
   description = "The name of the RDS database"
   type        = string
